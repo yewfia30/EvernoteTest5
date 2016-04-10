af8f34400cfce67b51889be61b1ef28accd78dd4
yewfia30 <yewfia30@gmail.com>
2016-04-10 12:57:09 +0900

    Initial Commit

---
+++ EvernoteTest5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EvernoteTest5
+//
+//  Created by yoshinobu on 2016/04/10.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EvernoteTest5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EvernoteTest5
+//
+//  Created by yoshinobu on 2016/04/10.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
